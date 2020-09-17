41c0bee739c390e08855c1fb69371fe3bb4802c8
Dominik Ringbauer <dominik.ringbauer@gmx.at>
2020-09-17 15:18:34 +0200

    advanced exercises

---
+++ Advanced.js
@@ -0,0 +1,78 @@
+/*var plants = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+
+var temp = new Array();
+
+temp = plants.split("/")
+
+document.write(temp[0] + "<br>" + "<br>" + temp[1] + "<br>" + "<br>" + temp[2] + "<br>" + "<br>" + temp[3] + "<br>" + "<br>" + temp[4] + "<br>" + "<br>" + temp[5] + "<br>" + "<br>" + temp[6] + "<br>" + "<br>" + temp[7]);
+*/
+
+//Advanced Exercises
+
+
+// Advanced 1
+
+/*var a = 7; b = 14; c = "21"; d = '36'; e = 42;
+
+var sum = a + b + c + d + e;
+
+console.log(sum)*/
+
+
+/*var a = 7; b = 14; c = 21; d = 36; e = 42;
+
+var sum = a + b + c + d + e;
+
+console.log(sum);
+
+var f = 1; g = 15; h = 8; i = 1;
+
+var sum2 = f * g * h * i;
+
+console.log(sum2);
+
+document.write(sum / sum2);*/
+
+
+//Advanced 2
+
+/*var numbers = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ];
+
+console.log(numbers[1][1]);
+console.log(numbers[4][2]);
+console.log(numbers[5][3]);
+console.log(numbers[2][3]);
+console.log(numbers[2][1]);*/
+
+var town = [["Vienna"], ["is"], ["a"], ["nice"], ["city"]];
+
+console.log(town[0] + "\n" + "&emsp;&emsp;&emsp;" + town[1] + "<br>" + "&emsp;&emsp;&emsp;&emsp;" + town[2] + "<br>" + "&emsp;&emsp;&emsp;&emsp;&emsp;" + town[3] + "<br>" + "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;" + town[4]);
+//document.write(town[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
